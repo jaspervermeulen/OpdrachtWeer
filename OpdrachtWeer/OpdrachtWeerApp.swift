881700d23cfc3a97be041563cad0a86ebb239153
Jasper <jaspervermeulen@icloud.com>
2025-03-20 09:43:48 +0100

    berichtje

---
+++ OpdrachtWeer/OpdrachtWeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpdrachtWeerApp.swift
+//  OpdrachtWeer
+//
+//  Created by Jasper Vermeulen on 20/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct OpdrachtWeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
